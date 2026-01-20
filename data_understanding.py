ce6d91c6a1374d25c13c109b91df42d9b7da91ee
vanithavini <vsvanithavini@gmail.com>
2026-01-21 01:18:40 +0530

    In this .py file to understand the raw dataset

---
+++ data_understanding.py
@@ -0,0 +1,43 @@
+import pandas as pd  
+import numpy as np   
+
+# Load the dataset
+df = pd.read_csv(r"C:\Users\Vanitha\OneDrive\Desktop\app_user_behavior_dataset.csv")
+
+# Quick Check
+print("Dataset Shape:", df.shape) 
+print("Columns:", df.columns.tolist())  
+print("First 5 rows:\n", df.head())  
+
+# Check data types
+print("Data types: ", df.info())
+
+# Summary statistics for numerical columns
+print("Summary Statistics for Numeric columns:\n ", df.describe())
+
+# Check missing values
+print("Missing values per column:\n", df.isnull().sum())
+
+# Check duplicates
+print("Number of duplicate rows:", df.duplicated().sum())
+
+# Categorical Value Distribution Check
+#for col in categorical_cols:
+ #   print(df[col].value_counts(normalize=True))
+ #   print("-" * 40)
+categorical_cols = [
+    'gender', 'country', 'device_type',
+    'app_version', 'subscription_type',
+    'marketing_source'
+]
+
+for col in categorical_cols:
+    print(f"\n{col.upper()} DISTRIBUTION")
+    print(df[col].value_counts(normalize=True))
+
+# Save a raw copy of data
+df.to_csv(
+    r"C:\Users\Vanitha\OneDrive\Documents\GUVI\capstone_project\app_user_behavior_segmentation\data\raw\raw_data.csv",
+    index=False
+)
+
